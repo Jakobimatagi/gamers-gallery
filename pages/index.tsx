6ae31cc2e7d31ca9ba37a62b6ce5167cc85257f2
Kobi Matagi <jkmatagi@hotmail.com>
2022-08-24 22:00:57 -0600

    avatar from material ui

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import Feed from './components/Feed'
+import Header from './components/Header'
+
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+export default Home
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,23 +1,29 @@
-import React from 'react'
-import { Container } from '@chakra-ui/react'
-import { BellIcon, EmailIcon} from '@chakra-ui/icons'
+import { Container } from "@chakra-ui/react";
+import Avatar from "@mui/material/Avatar";
+import { BellIcon, EmailIcon } from "@chakra-ui/icons";
+import styles from './navBar.module.css';
 function NavBar() {
   return (
-    <div >
-        <Container maxW="lg" mx="auto" px="4" py="4" bg={"#0C002A"}>
-
+    <div>
+      <Container maxW="lg" mx="auto" px="4" py="4" bg={"#0C002A"}>
         {/* user section */}
-        <div className='user-section'>
-        <BellIcon w={24} h={24} color='#CACED3' />
-        <EmailIcon w={24} h={24} color='#CACED3' />
+        <div className={styles.userSection}>
+          <div className={styles.userAvatar}>
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+            <div className={styles.userName}>
+              <h3>Rick James</h3>
+            </div>
+          </div>
+          <div className={styles.userIcons}>
+            <BellIcon marginLeft={24} w={24} h={24} color="#CACED3" />
+            <EmailIcon marginLeft={24} w={24} h={24} color="#CACED3" />
+          </div>
         </div>
         {/* search and icons */}
-        <div className='search'>
-            Search
-        </div>
-        </Container>
+        <div className="search">Search</div>
+      </Container>
     </div>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
