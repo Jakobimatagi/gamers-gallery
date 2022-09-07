00fa55bcff5a6d274d49eac570b8281b58752c1c
kobi matagi <jakobi.matagi@divvypay.com>
2022-09-07 14:28:38 -0600

    user tile

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
+import UserInfoDisplay from './components/UserInfoDisplay/UserInfoDisplay';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-
+      <UserInfoDisplay />
     </div>
   );
 }
